ca9bd77da4063528c73fe9759c0144272e613903
EduardoRodrigues123 <121134383+EduardoRodrigues123@users.noreply.github.com>
2023-07-04 14:25:01 -0300

    Update main.js

--- js/main.js
+++ js/main.js
@@ -23,16 +23,15 @@ form.addEventListener("submit", (evento) =>{
     if(existe){
       itemAtual.id = existe.id
       atualizaElemento(itemAtual)
+
+      itens[itens.findIndex(elemento => elemento.id === existe.id)] = itemAtual
     }else{
-      itemAtual.id = itens.length
+      itemAtual.id = itens[itens.length - 1] ? (itens[itens.length-1]).id + 1 : 0;
 
       criaElemento(itemAtual)
 
       itens.push(itemAtual)
     }
-
-
-     
 
     localStorage.setItem("itens", JSON.stringify(itens))
 
@@ -51,6 +50,8 @@ function criaElemento (item) {
   novoItem.appendChild(numeroItem) 
 
   novoItem.innerHTML += item.nome
+
+  novoItem.appendChild(botaoDeleta(item.id))
   
   lista.appendChild(novoItem)
   
@@ -59,4 +60,25 @@ function criaElemento (item) {
 
 function atualizaElemento (item){
   document.querySelector("[data-id='"+item.id+"']").innerHTML = item.quantidade
+}
+
+function botaoDeleta (id){
+  const elementoBotao = document.createElement("Button")
+  elementoBotao.innerText = "x"
+
+  elementoBotao.addEventListener("click", function() {
+    deletaElemento(this.parentNode, id)
+  })
+
+  return elementoBotao;
+}
+
+function deletaElemento(tag,id){
+  tag.remove()
+   
+  itens.splice(itens.findIndex(elemento => elemento.id === id),1)
+
+  console.log(itens)
+
+  localStorage.setItem("itens", JSON.stringify(itens))
 }
